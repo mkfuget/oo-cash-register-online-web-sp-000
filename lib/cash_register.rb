994ee0de9725d970b63992e36f968ed37f682016
Michael Fuget <michaelfuget@gmail.com>
2020-12-03 02:19:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,20 @@
 class CashRegister
   attr_accessor :total 
+  attr_accessor :transaction_list
   def initialize 
-    @total = 0 
+    @total = 0.0 
+    @transaction_list = []
   end
+  
+  def add_item(item, price, quantity = 1)
+    quantity.times do
+      @total += price
+      @transaction_list.push(
+          {
+            :item => item,
+            :price => price
+          }
+        )
+      end
+    end
 end 
